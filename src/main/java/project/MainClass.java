5a2454db61eb5240e7de8e1d72044dbbef52c649
Andrey Kravchenko <andreykravchenkodeveloper@gmail.com>
2022-01-23 19:31:51 +0300

    Commit 2

--- src/main/java/project/MainClass.java
+++ src/main/java/project/MainClass.java
@@ -2,11 +2,6 @@ package project;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
